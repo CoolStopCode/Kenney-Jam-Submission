4e7c912f51400991fba1508397142c722980c827
NewPlayer_43507 <147733146+NewPlayer-43507@users.noreply.github.com>
2025-07-18 22:34:30 -0400

    Fixed charger and failing to load issue
    bad git, github desktop better

--- game/Charger.gd
+++ game/Charger.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
-var Player = null
+var Player:bool = false
 
 func _physics_process(delta: float) -> void:
-	if Player != null:
-		Player.Power += 0.5
+	if Player and Global.power <= 100:
+		Global.power += 4 * delta
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		Player = body
+		Player = true
 
 func _on_body_exited(body: Node2D) -> void:
-	if body == Player:
-		Player = null
+	if body.is_in_group("Player"):
+		Player = false
